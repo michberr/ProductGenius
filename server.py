aa1443a845ff5b25a9b9117787ca6c41b1e69510
Michelle Berry <michberr8@gmail.com>
2017-02-17 17:58:29 -0800

    Built review histogram on product details page
    - quite curious that all the product have very high ratings.
    Need to look at data being seeded to determine why.
    - SQL code breaks if you search by two terms

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from jinja2 import StrictUndefined
 from indexes import INDEXES
 from model import Product, Review, User, connect_to_db, db
 from sqlalchemy import desc
+import json
 
 app = Flask(__name__)
 
@@ -61,6 +62,43 @@ def search_products():
         return render_template("no_products.html")
 
 
+@app.route('/product-reviews/<asin>.json')
+def product_reviews_data(asin):
+    """Return data about product reviews for chart."""
+
+    scores = Product.query.filter_by(asin=asin).one().scores
+
+    scores = json.loads(scores)
+
+    score_list = [scores["1"], scores["2"], scores["3"], scores["4"], scores["5"]]
+
+    data_dict = {
+                "labels": ["1", "2", "3", "4", "5"],
+                "datasets": [
+                    {
+                        "label": "Customer Ratings",
+                        "data": score_list,
+                        "backgroundColor": [
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(153, 102, 255, 0.6)'
+                        ],
+                        "hoverBackgroundColor": [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)'
+                        ],
+                        "borderWidth": 5
+                    }]
+            }
+
+    return jsonify(data_dict)
+
+
 @app.route('/search-review')
 def search_reviews():
     """Search through reviews for a product and display the results"""
@@ -90,9 +128,6 @@ def display_product_profile(asin):
 
     product = Product.query.filter_by(asin=asin).one()
     reviews = Review.query.filter_by(asin=asin).order_by(desc(Review.score)).all()
-
-    print product
-    print reviews
 
     return render_template("product_details.html",
                            product=product,
