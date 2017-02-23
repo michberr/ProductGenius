946d901f17869170e9fc95d580ec4eac8b22e282
Michelle Berry <michberr8@gmail.com>
2017-02-22 18:41:25 -0800

    Changed layout of product listing and details pages

--- product_genius.py
+++ product_genius.py
@@ -86,20 +86,8 @@ def get_chart_data(score_list):
         "datasets": [{
             "label": "Customer Ratings",
             "data": score_list,
-            "backgroundColor": [
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(54, 162, 235, 0.6)',
-                'rgba(255, 206, 86, 0.6)',
-                'rgba(75, 192, 192, 0.6)',
-                'rgba(153, 102, 255, 0.6)'
-            ],
-            "hoverBackgroundColor": [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
+            "backgroundColor": 'rgba(54, 162, 235, 0.6)',
+            "hoverBackgroundColor": 'rgba(54, 162, 235, 1)',
             "borderWidth": 5
         }]
     }
